e7cb8c4127750a4508d0bcfc614a1ebfb5f56d43
Roman <kaboom.ua@gmail.com>
2016-12-21 23:01:25 +0200

    Update toggleOpenArticle.js

--- src/decorators/toggleOpenArticle.js
+++ src/decorators/toggleOpenArticle.js
@@ -4,7 +4,9 @@ import React from 'react'
 export default function toggleOpenArticle(Component) {
     return class WrapperComponent extends React.Component {
         state = {
+            //суть декораторов в переисползовании кода, не привязывайся к названиям сущностей. Лучше openItemId
             openArticleId: null,
+            //не понял зачем это тебе нужно
             prevOpenedArticle: null
         }
         render() {
@@ -12,6 +14,7 @@ export default function toggleOpenArticle(Component) {
         }
 
         toggleOpenArticle = (id) => {
+            //тебе не нужно prevOpenedArticle - хватит openArticleId != id
             if (this.state.prevOpenedArticle != id)
                 this.setState({
                     prevOpenedArticle: this.state.openArticleId
