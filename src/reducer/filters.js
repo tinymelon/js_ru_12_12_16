672292db91f9a16416dc94a6fa5f2ef59f3cc8df
Roman <kaboom.ua@gmail.com>
2017-01-11 22:43:21 +0200

    Update filters.js

--- src/reducer/filters.js
+++ src/reducer/filters.js
@@ -5,7 +5,9 @@ export default (filtersState = {}, action) => {
 
     switch (type) {
         case SET_LABELS:
+            //лучше используй Objrct.assign, иначе замучаешься добавлять фильтры
             return {
+                //почему labels, а не ids?
                 labels: payload.labels,
                 date: filtersState.date
             }
